234d729fa44f1febcf78e3de5319e7f75327bb83
Dat Huynh Su <sudat.huynh@stud.h-da.de>
2024-08-20 07:44:43 +0700

    get node from site

--- adapter/Parser.py
+++ adapter/Parser.py
@@ -2,7 +2,6 @@ from typing import Set, List
 
 from adapter.utils import get_string_between_delimiters
 
-
 def extractNodeLabel(line, delimiters) -> List[str]:
     for delimiter in delimiters:
         firstDelimiter, secondDelimiter = delimiter
@@ -42,22 +41,28 @@ def extractNodesFromLinks(mermaidCode: str, mermaid_links_types: Set[str]):
     nodes = []
 
     for line in mermaid_code_as_list:
-        linkType = isLineContainsLink(line, mermaid_links_types)
-        if linkType:
-            leftNode = line.split(linkType)[0].strip()
-            rightNode = line.split(linkType)[1].strip()
-            if leftNode not in nodes:
-                nodes.append(leftNode)
-            if rightNode not in nodes:
-                nodes.append(rightNode)
+        while link_type := isLineContainsLink(line, mermaid_links_types):
+            left_nodes = line.split(link_type, 1)[0].split(" ")
+            right_sides, line = line.split(link_type, 1)[1], right_sides # there maybe more links involved 
+
+            for node in left_nodes:
+                if node not in nodes: nodes.append(node)
+            
+        for node in line:
+            if node not in nodes: nodes.append(node)
 
     return nodes
 
 def isLineContainsLink(line: str, mermaid_links_types: Set[str]):
-    for link_type in mermaid_links_types:
-        if link_type in line:
-            return link_type
-    return False
+    for i in range(0, len(line) - 3):
+        for link_type in mermaid_links_types:
+            if link_type in line[i: i + 3]:
+                return link_type
+    return None
+
+def getNodeFromSite(site: str):
+    return [node.strip() for node in site.split("&")]
+    
 
 #TODO: Handle complex mermaid links
 def extractEdgesFromMermaid(mermaidCode: str, mermaid_links_types: Set[str]):
